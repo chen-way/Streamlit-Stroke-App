867ed5dc60866864b9e5cc85515353081b790ed2
chen-way <panchenwei2010@gmail.com>
2025-02-02 19:39:05 -0700

    Update stroke.py

--- stroke.py
+++ stroke.py
@@ -202,7 +202,7 @@ def main():
         st.session_state.prediction_result
 
     st.header("💡 Health Tips")
-    st.markdown("### Check this out!")
+    st.markdown("#### 📢 Check this out! <br> https://www.health.harvard.edu/womens-health/8-things-you-can-do-to-prevent-a-stroke")
 
     col1, col2 = st.columns(2)
     
